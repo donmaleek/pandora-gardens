00c3ac915c3cbd230acc807f2677e662d5b83dea
donmaleek <bamboloshari@gmail.com>
2025-03-06 19:57:46 +0300

    setting up frontend infrastructure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-900 text-white text-4xl">
+      Pandora Gardens
+    </div>
+  )
+}
+
+export default App;
