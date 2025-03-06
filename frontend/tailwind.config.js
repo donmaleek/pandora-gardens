e7af5b9da74708dcd969095cf2ba6c310a7c18f3
donmaleek <bamboloshari@gmail.com>
2025-03-06 23:23:34 +0300

    Testing the frontend and backend intergration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
       './src/**/*.{js,ts,jsx,tsx}'
     ],
     theme: {
-      extend: {},
+      extend: {
+        animation: {
+          'fade-in': 'fadeIn 0.5s ease-in',
+          'spin': 'spin 1s linear infinite'
+        },
+
+        keyframes: {
+          fadeIn: {
+            '0%': { opacity: '0' },
+            '100%': { opacity: '1' },
+          }
+        }
+      },
     },
     plugins: [],
   }
